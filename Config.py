a34d7d742cdb555e131603676c8ffd586d2bdb02
Suraj kkumar chauhan <surajkc494@gmail.com>
2026-04-19 21:18:03 +0530

    Adapt pipeline for Telugu grapheme-level CTC OCR

--- Config.py
+++ Config.py
@@ -1,38 +1,43 @@
 import argparse
 
 
-class Configs():
+class Configs:
     def __init__(self):
         self.parser = argparse.ArgumentParser()
-        
-        # Data Creation
-        self.parser.add_argument('--train_words', type=int, help='The number of training words to be generated')
-        self.parser.add_argument('--valid_words', type=int, help='The number of validation words to be generated')
-        self.parser.add_argument('--test_words', type=int, help='The number of testing words to be generated')
-        
-        # Train
-        self.parser.add_argument('--data_path', type=str, help='specify your data path, better ending with the "/" ')
-        self.parser.add_argument('--img_height', type=int, default=64, help= "the size of the input images (height)")
-        self.parser.add_argument('--img_width', type=int, default=256, help= "the size of the input images (width)")
-        self.parser.add_argument('--vit_patch_size', type=int, default=16 , help=" better be a multiple of 2 like 8, 16 etc ..")
-        self.parser.add_argument('--max_text_len', type=int, default=20, help='the maximum word lenght, in our case 10 + 2, the added 2 are start token, end token')
-        self.parser.add_argument('--train_type', type=str , choices=['normal','stn','htr_Augm'], help="specify the desired transformation to be applied during training")
-        self.parser.add_argument('--batch_size', type=int, default=8)
-        self.parser.add_argument('--weights_path', type=str, default='./' , help='specify the path to save your weights "/" ')
 
-        # Pre-train
-        self.parser.add_argument('--vis_results', type=bool, default=True, help='specify if you want to visualise the recovering results or not')
+        # Dataset / Telugu OCR config
+        self.parser.add_argument('--language', type=str, default='telugu')
+        self.parser.add_argument('--charset', type=str, default='telugu_graphemes')
+        self.parser.add_argument('--data_path', type=str, default='./data/', help='Dataset root path')
+        self.parser.add_argument('--image_dir', type=str, default='./images/', help='Path with .jpg word images')
+        self.parser.add_argument('--labels_csv', type=str, default='labels.csv')
+        self.parser.add_argument('--vocab_file', type=str, default='vocab.txt')
+        self.parser.add_argument('--train_file', type=str, default='train.txt')
+        self.parser.add_argument('--val_file', type=str, default='valid.txt')
+        self.parser.add_argument('--test_file', type=str, default='test.txt')
 
-        # Fine-tune
-        self.parser.add_argument('--pretrained_encoder_path', type=str, default='./' , help='specify the path of your pretrained encoder to be loaded "/" ')
-        
-        # Test 
-        self.parser.add_argument('--test_model', type=str, help='specify the path of the model to be loaded for testing')
+        self.parser.add_argument('--img_height', type=int, default=64)
+        self.parser.add_argument('--img_width', type=int, default=512)
+        self.parser.add_argument('--max_width', type=int, default=512)
+        self.parser.add_argument('--vit_patch_size', type=int, default=16)
+        self.parser.add_argument('--max_text_len', type=int, default=64)
+
+        # Training / runtime
+        self.parser.add_argument('--train_type', type=str, default='normal', choices=['normal', 'stn', 'htr_Augm'])
+        self.parser.add_argument('--batch_size', type=int, default=32)
+        self.parser.add_argument('--num_workers', type=int, default=4)
+        self.parser.add_argument('--epochs', type=int, default=100)
+        self.parser.add_argument('--lr', type=float, default=1.5e-4)
+        self.parser.add_argument('--weights_path', type=str, default='./weights/')
+        self.parser.add_argument('--test_model', type=str, default='')
+        self.parser.add_argument('--pretrained_encoder_path', type=str, default='')
+
+        # Auto-computed at runtime from vocab
+        self.parser.add_argument('--num_classes', type=int, default=1246)
+        self.parser.add_argument('--blank_index', type=int, default=1245)
+
+        # Pre-train legacy switches
+        self.parser.add_argument('--vis_results', type=bool, default=True)
 
     def parse(self):
         return self.parser.parse_args()
-
-
-
-
-
